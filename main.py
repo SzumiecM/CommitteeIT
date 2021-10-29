782cea140285f65a15f60eedccaf4cf9e7ee52eb
SzumiecM <milosz.sz.m@gmail.com>
2021-10-29 22:20:53 +0200

    fully add hybrid assembler

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from assemblers.hybrid_assembler import HybridAssembler
 from xlsx_reader import EmployeesReader, ThesisReader
 from assemblers.genetic_assembler import GeneticAssembler
 from assemblers.heuristic_assembler import HeuristicAssembler
@@ -10,21 +11,26 @@ thesis_reader.map_employees(employee_reader.employees)
 assembler_params = {
     'thesis': thesis_reader.thesis,
     'employees': employee_reader.employees,
-    'slots': employee_reader.slots,
     'employees_per_slot': 3,
-    'population_count': 10
+    'population_count': 10,
+    'max_slots_per_employee': True,
+    'max_thesis_per_slot': 5
 }
 
 genetic_assembler = GeneticAssembler(
     **assembler_params,
-    max_thesis_per_slot=5,
     iteration_count=10,
-    max_slots_per_employee=True
 )
 
 heuristic_assembler = HeuristicAssembler(
     **assembler_params
 )
 
+hybrid_assembler = HybridAssembler(
+    **assembler_params,
+    iteration_count=10
+)
+
 # genetic_assembler.assemble()
-heuristic_assembler.assemble()
+# heuristic_assembler.assemble()
+hybrid_assembler.assemble()
